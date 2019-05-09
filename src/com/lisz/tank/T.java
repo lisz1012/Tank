9736d10562ae4a7b5964691b10623bd53e5f498f
lisz1012 <lisz1012@163.com>
2019-05-08 21:52:16 -0700

    Create a separatte TankFrame

--- src/com/lisz/tank/T.java
+++ src/com/lisz/tank/T.java
@@ -1,21 +1,8 @@
 package com.lisz.tank;
 
-import java.awt.Frame;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 public class T {
 	public static void main(String[] args) {
-		Frame f = new Frame();
-		f.setSize(800, 600);
-		f.setResizable(false);
-		f.setTitle("Tank War");
-		f.setVisible(true);
-		f.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		});
+		TankFrame tf = new TankFrame();
+		
 	}
 }
