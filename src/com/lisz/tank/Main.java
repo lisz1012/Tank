0fbec594ccf658d8cfa88041e9a5e3504b79d091
lisz1012 <lisz1012@163.com>
2019-05-09 21:29:29 -0700

    Make the black square moveable

--- src/com/lisz/tank/Main.java
+++ src/com/lisz/tank/Main.java
@@ -3,6 +3,5 @@ package com.lisz.tank;
 public class Main {
 	public static void main(String[] args) {
 		TankFrame tf = new TankFrame();
-		
 	}
 }
--- src/com/lisz/tank/TankFrame.java
+++ src/com/lisz/tank/TankFrame.java
@@ -1,7 +1,10 @@
 package com.lisz.tank;
 
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -21,12 +24,29 @@ public class TankFrame extends Frame {
 				System.exit(0);
 			}
 		});
+		addKeyListener(new MyKeyListener());
 	}
 	
 	@Override
 	public void paint(Graphics g) {
+		g.setColor(new Color(255, 255, 255));
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.setColor(new Color(0, 0, 0));
 		g.fillRect(x, y, 50, 50);
-		x += 10;
-		y += 10;
+	}
+	
+	private class MyKeyListener extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_UP: y -= 10; break;
+			case KeyEvent.VK_DOWN: y += 10; break;
+			case KeyEvent.VK_LEFT: x -= 10; break;
+			case KeyEvent.VK_RIGHT: x += 10; break;
+			default:
+				break;
+			}
+			paint(getGraphics());
+		}
 	}
 }
