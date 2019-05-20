15edf22e20be47c1f2c6ea19281f614a96b006d3
lisz1012 <lisz1012@163.com>
2019-05-20 16:19:53 -0700

    Adjust the initial position of the bullets

--- src/com/lisz/tank/DefaultCannon.java
+++ src/com/lisz/tank/DefaultCannon.java
@@ -4,11 +4,12 @@ public class DefaultCannon implements Cannon {
 
 	@Override
 	public void fire(Tank tank) {
-		if (tank.getDir() == Dir.RIGHT_UP || tank.getDir() == Dir.LEFT_DOWN) {
+		/*if (tank.getDir() == Dir.RIGHT_UP || tank.getDir() == Dir.LEFT_DOWN) {
 			tank.tf.gameObjects.add(new Bullet(tank.getDir(), tank.getX() + Tank.WIDTH / 2 + 15, tank.getY() + Tank.HEIGHT / 2 - Bullet.HEIGHT / 2, tank.tf, tank.group));
 		} else {
 			tank.tf.gameObjects.add(new Bullet(tank.getDir(), tank.getX() + Tank.WIDTH / 2 - Bullet.WIDTH / 2, tank.getY() + Tank.HEIGHT / 2 - Bullet.HEIGHT / 2, tank.tf, tank.group));
-		}
+		}*/
+		tank.tf.gameObjects.add(new Bullet(tank.getDir(), tank.getX() + Tank.WIDTH / 2 - Bullet.WIDTH / 2, tank.getY() + Tank.HEIGHT / 2 - Bullet.HEIGHT / 2, tank.tf, tank.group));
 	}
 
 }
