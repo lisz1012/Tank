88b2b111182f855da31626f64d88ac9a04b91006
lisz1012 <lisz1012@163.com>
2019-07-15 00:44:20 -0700

    Add notes

--- src/com/lisz/tank/net/MessageEncoder.java
+++ src/com/lisz/tank/net/MessageEncoder.java
@@ -18,6 +18,7 @@ public class MessageEncoder extends MessageToByteEncoder<Message> {
 	@Override
 	protected void encode(ChannelHandlerContext ctx, Message msg, ByteBuf out) throws Exception {
 		// 依次写入Type和size，后面client接收到的时候根据type作出反应；而且在size不定的情况下这种写法比Message里面的SIZE常量更灵活
+		// 这样些比较底层的网络协议的定义，这样可以跨语言
 		out.writeInt(msg.getType().ordinal());
 		byte[] bytes = msg.toBytes();
 		int length = bytes.length;
