d65b877b80ce664df07588b71724d007aa11df03
lisz1012 <lisz1012@163.com>
2019-07-17 20:44:57 -0700

    Add notes and disable the TCP Nagle algorithm

--- src/com/lisz/tank/net/Server.java
+++ src/com/lisz/tank/net/Server.java
@@ -3,6 +3,7 @@ package com.lisz.tank.net;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
@@ -24,6 +25,7 @@ public class Server {
 		ServerBootstrap b = new ServerBootstrap();
 		b.group(bossGroup, workerGroup)
 		 .channel(NioServerSocketChannel.class)
+		 .option(ChannelOption.TCP_NODELAY, true) // 禁用Nagle算法防止卡顿和收包不及时
 		 .childHandler(new ChannelInitializer<SocketChannel>() {
 			@Override
 			protected void initChannel(SocketChannel ch) throws Exception {
